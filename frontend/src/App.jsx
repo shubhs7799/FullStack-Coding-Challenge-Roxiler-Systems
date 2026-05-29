63b9f8ec1c5b7766f8547dc861ffc12512904469
shubhs7799 <rshubham928@gmail.com>
2026-05-29 22:26:59 +0530

    chore(frontend): setup Vite + React + Tailwind CSS + React Router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  const [time,setTime] = useSate(0)
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
